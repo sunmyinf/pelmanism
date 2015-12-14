62d02b26fae1a2206238a3a64f34ac69e6353d59
shalman <nao0101.n@gmail.com>
2015-12-14 10:22:57 +0900

    add basic life cycle

--- pelmanism.c
+++ pelmanism.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
 int main(){
-	
+	int card[10] = {0,1,2,3,4,5,6,7,8,9};
+	int block[10] = {}
+	//配列シャッフル
+	int firstNum;
+	int secondNum;
+
+	for(;;){
+		printf("■　■　■　■　■　■　■　■　■　■\n");
+		printf("好きなカードを選んでください(左から0,1,2,3,4,5,6,7,8,9に対応しています。\n");
+		scanf("%d",&firstNum);
+		printf("もう一つ選んでください\n");
+		scanf("%d",&secondNum);
+		printf("%d,%d\n",firstNum,secondNum);
+
+	}
 }
